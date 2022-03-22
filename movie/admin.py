3d1917e1d23076cb8b522834493aed2e97885004
Miguel Quintana <miqo1385@gmail.com>
2022-03-21 21:53:03 -0700

    models commit

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import MovieType, Movie, Review
 # Register your models here.
+admin.site.register(MovieType)
+admin.site.register(Movie)
+admin.site.register(Review)
--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class MovieType(models.Model):
+    moviegender=models.CharField(max_length=255)
+    moviedescription=models.TextField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.moviegender
+    class Meta:
+        db_table='movietype'
+
+
+class Movie(models.Model):
+    moviename=models.CharField(max_length=255)
+    moviegender=models.ForeignKey(MovieType, on_delete=models.DO_NOTHING )
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    dateentered=models.DateField()
+    datemoviereleased=models.DateField()
+    moviebudget=models.DecimalField(max_digits=10, decimal_places=2)
+    producturl=models.URLField()
+
+    def __str__(self):
+        return self.moviename
+
+    class Meta:
+        db_table='movie'
+
+
+class Review(models.Model):
+    movietitle=models.CharField(max_length=255)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    reviewdate=models.DateField()
+    reviewtext=models.TextField()
+
+    def __str__(self):
+        return self.movietitle
+
+    class Meta:
+        db_table='review'
+
+
+

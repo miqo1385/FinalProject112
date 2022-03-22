dadd67c4b50ac5a43fdbb48e2604767b985b3932
Miguel Quintana <miqo1385@gmail.com>
2022-03-22 11:36:49 -0700

    commit  product view

--- movie/views.py
+++ movie/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import MovieType, Movie, Review
 
 # Create your views here.
 def index(request):
     return render(request, 'movie/index.html')
+
+def movies(request):
+    movie_list=Movie.objects.all()
+    return render(request, 'movie/moviereview.html', {'movie_list': movie_list} )

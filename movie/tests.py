20c912f48ed04f8560c54b47ce072b36c04d3e6d
Miguel Quintana <miqo1385@gmail.com>
2022-03-22 21:59:16 -0700

    test 1 commit

--- movie/tests.py
+++ movie/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import MovieType, Movie, Review
+import datetime
 
 # Create your tests here.
+class MovieTypeTest(TestCase):
+    def setUp(self):
+        self.type=MovieType(moviegender='Romantic')
+    
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'Romantic')
+
+    def test_tablename(self):
+        self.assertEqual(str(MovieType._meta.db_table), 'movietype')
+
+class MovieTest(TestCase):
+    def setUp(self):
+        self.type=MovieType(moviegender='Romantic')
+        self.user=User(username='userOne')
+        self.movie=Movie(moviename='Titanic', user=self.user, dateentered=datetime.date(2022,3,22), datemoviereleased=datetime.date(1997,12,25), moviebudget=200.00, producturl='https://en.wikipedia.org/wiki/Titanic_(1997_film)' )
+    
+    def test_string(self):
+        self.assertEqual(str(self.movie), 'Titanic')
